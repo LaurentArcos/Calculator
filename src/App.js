9a071d36fa79474f513b206cbc43a1b6d3d72327
LaurentArcos <laurent.arcos@gmail.com>
2023-03-06 15:13:23 +0100

    button and button box components created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react'
 import Screen from './components/Screen'
 import Wrapper from './components/Wrapper'
 
+const buttonValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9,"x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+]
+
 const App = () => {
   return (
     <div>
       <Wrapper>
            <Screen/>
-           <h1>App</h1>
+           <
       </Wrapper>
     </div>
   )
