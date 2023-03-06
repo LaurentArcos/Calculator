5a080533557ebf72ab694d464c23d84e875b2371
LaurentArcos <laurent.arcos@gmail.com>
2023-03-06 15:09:54 +0100

    screen component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Screen from './components/Screen'
 import Wrapper from './components/Wrapper'
 
 const App = () => {
   return (
     <div>
       <Wrapper>
+           <Screen/>
            <h1>App</h1>
       </Wrapper>
     </div>
